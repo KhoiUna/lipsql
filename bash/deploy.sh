05cd5753fbda47344caee6e528c066b09c210c8c
KhoiUna <thorwaitson@gmail.com>
2025-08-23 00:55:19 -0500

    fix bash

--- bash/deploy.sh
+++ bash/deploy.sh
@@ -3,3 +3,5 @@
 docker compose up --build -d --remove-orphans
 
 docker system prune --all --volumes --force
+
+curl -X POST -H "Content-Type: application/json" -d '{"content": "PlainSQL: Docker build done!"}' https://discord.com/api/webhooks/1311081117553655950/dhPOwv8W-_ZXdZNoRkfdhGKcSvE0NjMZkcQbDYrfXxuOGyjlNbiUJmTeANW65rjZcAcg
