6be199f23a78fa7a0ecfea2a06fb40c15cb7febd
KhoiUna <thorwaitson@gmail.com>
2025-08-28 19:22:28 -0500

    modified:   middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -41,6 +41,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
 	matcher: [
-		'/((?!api|_next/static|_next/image|.*\\.png$|manifest\\.json).*)',
+		'/((?!api|_next/static|_next/image|.*\\.png$|.*\\.ico$|manifest\\.json).*)',
 	],
 };
