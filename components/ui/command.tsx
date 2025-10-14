866e687aed9489de82a9be50685cbebce88de4cc
khoiuna <thorwaitson@gmail.com>
2025-10-14 10:39:21 -0500

    modified:   components/ui/command.tsx

--- components/ui/command.tsx
+++ components/ui/command.tsx
@@ -25,7 +25,7 @@ const CommandInput = React.forwardRef<
 	<CommandPrimitive.Input
 		ref={ref}
 		className={cn(
-			'flex h-11 w-full rounded-md bg-transparent py-3 text-sm outline-none placeholder:text-gray-400 disabled:cursor-not-allowed disabled:opacity-50',
+			'flex h-11 w-full rounded-md bg-transparent p-2 text-sm outline-none placeholder:text-gray-400 disabled:cursor-not-allowed disabled:opacity-50',
 			className
 		)}
 		{...props}
